e1a0cfb91b24c225b5eb91c9d4394838b4805fa8
Waqqas Meraj <waqas.meraj@carameltec.com>
2018-04-16 19:45:05 +0500

    updated script for multiple clients

--- benchmarks/throughputCounter.js
+++ benchmarks/throughputCounter.js
@@ -2,21 +2,36 @@
 
 var mqtt = require('../')
 
-var client = mqtt.connect({ port: 1883, host: 'localhost', clean: true, encoding: 'binary', keepalive: 0 })
-var counter = 0
-var interval = 5000
 
-function count () {
-  console.log('received/s', counter / interval * 1000)
+let clients = [];
+let nc = 5000;
+for (let i = 0; i < nc; ++i) {
+  clients.push(mqtt.connect({ username: 'test', password: 'test', port: 80, host: 'mqtt.fizz.io', clean: true, encoding: 'binary', keepalive: 50 }))
+}
+
+let counter = 0
+let totalMessages = 0
+
+const interval = 1000
+
+let logCount = 0;
+setInterval(printStats, interval)
+function printStats () {
+  console.log(
+    logCount++,
+    Date(), 
+    'rcvd/s', counter / interval * 1000,  
+    'total', totalMessages
+  )
   counter = 0
 }
 
-setInterval(count, interval)
-
-client.on('connect', function () {
-  count()
-  this.subscribe('test')
-  this.on('message', function () {
-    counter++
-  })
-})
+for (let i = 0; i < nc; ++i) {
+  clients[i].on('connect', function () {
+    this.subscribe('test', {qos: 1})
+    this.on('message', function () {
+      counter++
+      totalMessages++;
+    })
+  })  
+}
